662049fb88401528b9e2faaa90c4a3912e0bd2c7
Dheba Ram Prajapati <104148571+DhebaRam@users.noreply.github.com>
2025-07-09 19:14:17 +0530

    Update SmsConsentForOtpAutofillPlugin.kt
    same code

--- android/src/main/kotlin/com/sms_consent_for_otp_autofill/sms_consent_for_otp_autofill/SmsConsentForOtpAutofillPlugin.kt
+++ android/src/main/kotlin/com/sms_consent_for_otp_autofill/sms_consent_for_otp_autofill/SmsConsentForOtpAutofillPlugin.kt
@@ -19,176 +19,172 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 import java.util.regex.Pattern
 import android.content.ComponentName
-import android.util.Log
-import com.google.android.gms.auth.api.identity.GetPhoneNumberHintIntentRequest
-import com.google.android.gms.auth.api.identity.Identity
+// import android.util.Log
+// import com.google.android.gms.auth.api.identity.GetPhoneNumberHintIntentRequest
+// import com.google.android.gms.auth.api.identity.Identity
 
 /** SmsConsentForOtpAutofillPlugin */
-class SmsConsentForOtpAutofillPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
-    private lateinit var channel: MethodChannel
-    private lateinit var mActivity: Activity
+class SmsConsentForOtpAutofillPlugin: FlutterPlugin, MethodCallHandler, ActivityAware {
+  private lateinit var channel: MethodChannel
+  private lateinit var mActivity: Activity
 
-    companion object {
-        private const val CREDENTIAL_PICKER_REQUEST = 100
-        private const val SMS_CONSENT_REQUEST = 200
-        private const val TAG = "SmsConsentPlugin"
-    }
+  companion object {
+    private const val CREDENTIAL_PICKER_REQUEST = 100
+    private const val SMS_CONSENT_REQUEST = 200
+       // private const val TAG = "SmsConsentPlugin"
+  }
 
-    override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-        channel = MethodChannel(flutterPluginBinding.binaryMessenger, "sms_consent_for_otp_autofill")
-        channel.setMethodCallHandler(this)
-    }
+  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "sms_consent_for_otp_autofill")
+    channel.setMethodCallHandler(this)
+  }
 
-    override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-        when (call.method) {
-            "requestPhoneNumber" -> {
-                requestPhoneHint()
-                result.success(null)
-            }
-            "requestSms" -> {
-                SmsRetriever.getClient(mActivity.applicationContext).startSmsUserConsent(call.argument<String>("senderPhoneNumber"))
+  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+    when (call.method) {
+      "requestPhoneNumber" -> {
+        requestHint()
+        result.success(null)
+      }
+      "requestSms" -> {
+        SmsRetriever.getClient(mActivity.applicationContext).startSmsUserConsent(call.argument<String>("senderPhoneNumber"))
 
 //        mActivity.registerReceiver(smsVerificationReceiver, IntentFilter(SmsRetriever.SMS_RETRIEVED_ACTION))
-                val filter = IntentFilter(SmsRetriever.SMS_RETRIEVED_ACTION)
-                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.TIRAMISU) {
-                    mActivity.registerReceiver(smsVerificationReceiver, filter, Context.RECEIVER_EXPORTED)
-                } else {
-                    mActivity.registerReceiver(smsVerificationReceiver, filter)
-                }
-                result.success(null)
-            }
+        val filter = IntentFilter(SmsRetriever.SMS_RETRIEVED_ACTION)
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.TIRAMISU) {
+          mActivity.registerReceiver(smsVerificationReceiver, filter, Context.RECEIVER_EXPORTED)
+        } else {
+          mActivity.registerReceiver(smsVerificationReceiver, filter)
         }
+        result.success(null)
+      }
     }
+  }
+  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+    channel.setMethodCallHandler(null)
+  }
 
-    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-        channel.setMethodCallHandler(null)
-    }
+  override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+    mActivity = binding.activity
 
-    override fun onAttachedToActivity(binding: ActivityPluginBinding) {
-        mActivity = binding.activity
+    binding.addActivityResultListener { requestCode, resultCode, data ->
+      when (requestCode) {
+        CREDENTIAL_PICKER_REQUEST -> {// Obtain the phone number from the result
+          if (resultCode == Activity.RESULT_OK && data != null) {
 
-        binding.addActivityResultListener { requestCode, resultCode, data ->
-            when (requestCode) {
-                CREDENTIAL_PICKER_REQUEST -> {// Obtain the phone number from the result
-                    if (resultCode == Activity.RESULT_OK && data != null) {
-
-                        val phoneNumber1 = Identity.getSignInClient(mActivity)
-                            .getPhoneNumberFromIntent(data)
-                        Log.d("SmsConsentPlugin", "📞 Selected Phone Number: $phoneNumber1")
-                        channel.invokeMethod("selectedPhoneNumber", phoneNumber1)
-                    } else {
-                        channel.invokeMethod("selectedPhoneNumber", null)
-                    }
-                    true
-                    /*if (resultCode == Activity.RESULT_OK && data != null) {
-                        channel.invokeMethod(
-                            "selectedPhoneNumber",
-                            data.getParcelableExtra<Credential>(Credential.EXTRA_KEY)?.id
-                        )
-                    } else {
-                        channel.invokeMethod("selectedPhoneNumber", null)
-                    }
-                    true*/
-                }
-
-                SMS_CONSENT_REQUEST -> {// Obtain the phone number from the result
-                    if (resultCode == Activity.RESULT_OK && data != null) {
-                        channel
-                            .invokeMethod(
-                                "receivedSms",
-                                parseOneTimeCode(data.getStringExtra(SmsRetriever.EXTRA_SMS_MESSAGE))
-                            )
-                        mActivity.unregisterReceiver(smsVerificationReceiver)
-                    } else {
-                        // Consent denied. User can type OTC manually.
-                        channel.invokeMethod("receivedSms", null)
-                        mActivity.unregisterReceiver(smsVerificationReceiver)
-                    }
-                    true
-                }
-
-                else -> false
-            }
+                    //     val phoneNumber1 = Identity.getSignInClient(mActivity)
+                    //         .getPhoneNumberFromIntent(data)
+                    //     Log.d("SmsConsentPlugin", "📞 Selected Phone Number: $phoneNumber1")
+                    //     channel.invokeMethod("selectedPhoneNumber", phoneNumber1)
+                    // } else {
+                    //     channel.invokeMethod("selectedPhoneNumber", null)
+                    // }
+                    // true
+          if (resultCode == Activity.RESULT_OK && data != null) {
+            channel.invokeMethod("selectedPhoneNumber", data.getParcelableExtra<Credential>(Credential.EXTRA_KEY)?.id)
+          } else {
+            channel.invokeMethod("selectedPhoneNumber", null)
+          }
+          true
         }
-    }
-
-    override fun onDetachedFromActivityForConfigChanges() {}
-
-    override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {}
-
-    override fun onDetachedFromActivity() {}
-
-    /// Construct a request for phone numbers and show the picker
-    private fun requestPhoneHint() {
-        val request = GetPhoneNumberHintIntentRequest.builder().build()
-        Identity.getSignInClient(mActivity)
-            .getPhoneNumberHintIntent(request)
-            .addOnSuccessListener { intentSender ->
-                try {
-                    Log.d("SmsConsentPlugin", "✅ Phone hint intent launched.")
-                    mActivity.startIntentSenderForResult(
-                        intentSender.intentSender,
-                        CREDENTIAL_PICKER_REQUEST,
-                        null, 0, 0, 0
-                    )
-                } catch (e: IntentSender.SendIntentException) {
-                    Log.e("SmsConsentPlugin", "❌ Failed to launch hint picker", e)
-                }
-            }
-            .addOnFailureListener { e ->
-                Log.e("SmsConsentPlugin", "❌ Failed to get hint intent", e)
-            }
-    }
-
-
-    private val smsVerificationReceiver = object : BroadcastReceiver() {
-        override fun onReceive(context: Context, intent: Intent) {
-            if (SmsRetriever.SMS_RETRIEVED_ACTION == intent.action) {
-                val extras = intent.extras
-                val smsRetrieverStatus = extras?.get(SmsRetriever.EXTRA_STATUS) as Status
-
-                when (smsRetrieverStatus.statusCode) {
-                    CommonStatusCodes.SUCCESS -> {
-                        // Get consent intent
-                        try {
-                            // Start activity to show consent dialog to user, activity must be started in
-                            // 5 minutes, otherwise you'll receive another TIMEOUT intent
-                            val consentIntent: Intent =
-                                extras.getParcelable(SmsRetriever.EXTRA_CONSENT_INTENT)
-                                    ?: //handle error
-                                    return
-                            val flags: Int = consentIntent.getFlags()
-                            val name: ComponentName =
-                                consentIntent.resolveActivity(mActivity.packageManager)
-
-                            if (name != null && name.packageName == "com.google.android.gms"
-                                && name.className == "com.google.android.gms.auth.api.phone.ui.UserConsentPromptActivity"
-                                && (flags and Intent.FLAG_GRANT_READ_URI_PERMISSION == 0)
-                                && (flags and Intent.FLAG_GRANT_WRITE_URI_PERMISSION == 0)
-                            ) {
-                                // Redirect the nested intent.
-                                mActivity.startActivityForResult(consentIntent, SMS_CONSENT_REQUEST)
-                            }
-                        } catch (e: ActivityNotFoundException) {
-                            // Handle the exception ...
-                        }
-                    }
-
-                    CommonStatusCodes.TIMEOUT -> {
-                        // Time out occurred, handle the error.
-                    }
-                }
-            }
+        SMS_CONSENT_REQUEST -> {// Obtain the phone number from the result
+          if (resultCode == Activity.RESULT_OK && data != null) {
+            channel
+              .invokeMethod("receivedSms", parseOneTimeCode(data.getStringExtra(SmsRetriever.EXTRA_SMS_MESSAGE)))
+            mActivity.unregisterReceiver(smsVerificationReceiver)
+          } else {
+            // Consent denied. User can type OTC manually.
+            channel.invokeMethod("receivedSms", null)
+            mActivity.unregisterReceiver(smsVerificationReceiver)
+          }
+          true
         }
+        else -> false
+      }
     }
+  }
 
-    private fun parseOneTimeCode(message: String?): String? {
-        val pattern = Pattern.compile("\\d{6,10}")
-        if (message != null) {
-            val matcher = pattern.matcher(message)
-            if (!matcher.find()) return message
-            return matcher.group(0)
+  override fun onDetachedFromActivityForConfigChanges() {}
+
+  override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {}
+
+  override fun onDetachedFromActivity() {}
+
+  /// Construct a request for phone numbers and show the picker
+    // private fun requestPhoneHint() {
+    //     val request = GetPhoneNumberHintIntentRequest.builder().build()
+    //     Identity.getSignInClient(mActivity)
+    //         .getPhoneNumberHintIntent(request)
+    //         .addOnSuccessListener { intentSender ->
+    //             try {
+    //                 Log.d("SmsConsentPlugin", "✅ Phone hint intent launched.")
+    //                 mActivity.startIntentSenderForResult(
+    //                     intentSender.intentSender,
+    //                     CREDENTIAL_PICKER_REQUEST,
+    //                     null, 0, 0, 0
+    //                 )
+    //             } catch (e: IntentSender.SendIntentException) {
+    //                 Log.e("SmsConsentPlugin", "❌ Failed to launch hint picker", e)
+    //             }
+    //         }
+    //         .addOnFailureListener { e ->
+    //             Log.e("SmsConsentPlugin", "❌ Failed to get hint intent", e)
+    //         }
+    // }
+  /// Construct a request for phone numbers and show the picker
+  private fun requestHint() {
+    mActivity.startIntentSenderForResult(
+      Credentials.getClient(mActivity).getHintPickerIntent(HintRequest.Builder()
+        .setPhoneNumberIdentifierSupported(true)
+        .build()).intentSender,
+      CREDENTIAL_PICKER_REQUEST,
+      null, 0, 0, 0
+    )
+  }
+
+  private val smsVerificationReceiver = object : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+      if (SmsRetriever.SMS_RETRIEVED_ACTION == intent.action) {
+        val extras = intent.extras
+        val smsRetrieverStatus = extras?.get(SmsRetriever.EXTRA_STATUS) as Status
+
+        when (smsRetrieverStatus.statusCode) {
+          CommonStatusCodes.SUCCESS -> {
+            // Get consent intent
+            try {
+              // Start activity to show consent dialog to user, activity must be started in
+              // 5 minutes, otherwise you'll receive another TIMEOUT intent
+              val consentIntent: Intent = extras.getParcelable(SmsRetriever.EXTRA_CONSENT_INTENT)
+                ?: //handle error
+                return
+              val flags: Int = consentIntent.getFlags()
+              val name: ComponentName = consentIntent.resolveActivity(mActivity.packageManager)
+
+              if (name !=null && name.packageName == "com.google.android.gms"
+                && name.className == "com.google.android.gms.auth.api.phone.ui.UserConsentPromptActivity"
+                && (flags and  Intent.FLAG_GRANT_READ_URI_PERMISSION == 0)
+                && (flags and  Intent.FLAG_GRANT_WRITE_URI_PERMISSION == 0)
+              ) {
+                // Redirect the nested intent.
+                mActivity.startActivityForResult(consentIntent, SMS_CONSENT_REQUEST)
+              }
+            } catch (e: ActivityNotFoundException) {
+              // Handle the exception ...
+            }
+          }
+          CommonStatusCodes.TIMEOUT -> {
+            // Time out occurred, handle the error.
+          }
         }
-        return null
+      }
     }
+  }
+  private fun parseOneTimeCode(message: String?): String? {
+    val pattern = Pattern.compile("\\d{6,10}")
+    if (message != null) {
+      val matcher = pattern.matcher(message)
+      if (!matcher.find()) return message
+      return matcher.group(0)
+    }
+    return null
+  }
 }
